7d983704497117380269397b8fe6972aaa0b0072
maeng2418 <maeng2418@github.com>
2018-12-31 02:55:48 +0900

    stateless component

--- src/movie.js
+++ src/movie.js
@@ -2,28 +2,26 @@ import React, { Component } from 'react';
 import './movie.css';
 import PropTypes from 'prop-types';
 
-class Movie extends Component{
-    //부모 컴포넌트에서 얻는 정보의 종류가 무엇인지, 있는지 없는지 알 수 있음.
-    static propTypes = {
-        title : PropTypes.string,
-        poster : PropTypes.string
-    }
-    render(){
-        return(
-            <div>
-                <MoviePoster poster = {this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie ({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster = {poster}/>
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
-    render(){
-        return(
-            <img src= {this.props.poster}/>
-        )
-    }
+function MoviePoster ({poster}){
+    return(
+        <img src= {poster} alt="Movie Poster"/>
+    )
+    
+}
+
+//부모 컴포넌트에서 얻는 정보의 종류가 무엇인지, 있는지 없는지 알 수 있음.
+Movie.propTypes = {
+    title : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired
 }
 
 export default Movie;
