4429a9f1cd508da508d569f81852c260ad94cb35
maeng2418 <maeng2418@github.com>
2018-12-31 01:46:25 +0900

    propTypes

--- src/movie.js
+++ src/movie.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import './movie.css';
+import PropTypes from 'prop-types';
 
 class Movie extends Component{
+    //부모 컴포넌트에서 얻는 정보의 종류가 무엇인지, 있는지 없는지 알 수 있음.
+    static propTypes = {
+        title : PropTypes.string,
+        poster : PropTypes.string
+    }
     render(){
         return(
             <div>
