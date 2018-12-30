86271ff3d308caf2f7018b9e9c7ef84a841530bf
maeng2418 <maeng2418@github.com>
2018-12-31 00:45:13 +0900

    컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './movie';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Movie /> {/*Movie 컴포넌트*/}
       </div>
     );
   }
