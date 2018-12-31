3cf41fe909a1f0f512acf0d3273fa960ae431feb
maeng2418 <maeng2418@naver.com>
2019-01-01 03:21:40 +0900

    Updates

---
+++ precache-manifest.670d4ba6b9c3e6585f984962ff2f8a5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "fc52ae5ef29f52c797ea",
+    "url": "/movie_app/static/js/main.fc52ae5e.chunk.js"
+  },
+  {
+    "revision": "3c9ecd7b1f95e1898d4a",
+    "url": "/movie_app/static/js/1.3c9ecd7b.chunk.js"
+  },
+  {
+    "revision": "fc52ae5ef29f52c797ea",
+    "url": "/movie_app/static/css/main.a8314bbe.chunk.css"
+  },
+  {
+    "revision": "d99d8e346b590810c2f873876b757d82",
+    "url": "/movie_app/index.html"
+  }
+];
